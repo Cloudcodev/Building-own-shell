2d95e986a6f4f44b56e3071e057a44b9282e44e3
salima <salimalk@Mac.fritz.box>
2026-03-29 22:23:55 +0200

    Fix: Add REPL loop to handle multiple invalid commands

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,6 @@ int main() {
   std::cout << std::unitbuf; //Forces the output to be printed immediately
   std::cerr << std::unitbuf; //used for error messages to show up on screen
 
-  std::cout << "$ "; //printing prompt
-
   //keeps the program alive to handle multiple commands.
   while (true) {
         std::cout << "$ ";
