770e120ee5a6034bc4916d2253e51ba2d22eff35
salima <salimalk@Mac.fritz.box>
2026-03-30 20:50:45 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,16 @@ int main() {
         // This check handles both "exit" and "exit 0".
         if (input == "exit 0" || input == "exit") {
             break;
-        }else if(input.substr(0,5) == "echo "){ // adding feature "echo"
-            std::cout << input.substr(5) << std::endl; //returns a substring starting from index 5
-        }else if (input == "echo ") {
-            std::cout << std::endl;
-        } else if(!input.empty()){
-            std::cout << input << ": command not found" << std::endl;
         }
+
+        //adding feature: "echo"
+        if (input.substr(0, 5) == "echo ") {
+        std::cout << input.substr(5) << std::endl;
+        continue; 
+    }
+        if (!input.empty()) {
+        std::cout << input << ": command not found" << std::endl;
+    }
   }
 
     return 0;
