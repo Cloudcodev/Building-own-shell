0a49c50a95e78c74f864dd2f621074718bc79d65
salima <salimalk@Mac.fritz.box>
2026-03-30 20:48:09 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -7,23 +7,20 @@ int main() {
 
     while (true) {
         std::cout << "$ ";
-
         std::string input;
-        if (!std::getline(std::cin, input)) {
-            break; 
-        }
+        std::getline(std::cin, input);
 
         // This check handles both "exit" and "exit 0".
         if (input == "exit 0" || input == "exit") {
             break;
+        }else if(input.substr(0,5) == "echo "){ // adding feature "echo"
+            std::cout << input.substr(5) << std::endl; //returns a substring starting from index 5
+        }else if (input == "echo ") {
+            std::cout << std::endl;
+        } else if(!input.empty()){
+            std::cout << input << ": command not found" << std::endl;
         }
-
-        if (input.empty()) {
-            continue;
-        }
-
-        std::cout << input << ": command not found" << std::endl;
-    }
+  }
 
     return 0;
 }
